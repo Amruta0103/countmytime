d1399c2f54c7268e73e722a06c04423166ee0348
Amruta_D <71976347+Amruta0103@users.noreply.github.com>
2023-07-28 12:01:21 +0530

    timer functions finalized

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,19 @@ import Timer from "./components/Timer";
 
 const App = () => {
   const App = styled.div`
+  display: flex;
   background-color : #282c34 ;
   color: white;
   display: flex;
   text-align : center;
   justify-content: center;
-  align-items:center;
+  align-items: center;
   height: 100vh;
   width: 100%;
   `
-  const Pomodoro = styled.div`
-  height: 80%;
-  width: 80%;
-  border-radius: 2rem;
-  // backdrop-filter: blur(20px);
-  // background: rgb(255, 255, 255, 0.2);
-  display: flex;
-  justify-content: center;
-  `
-
   return (
     <App>
-     <Pomodoro>
       <Timer />
-     </Pomodoro>
     </App>
   );
 }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,60 +2,70 @@ import { useState } from 'react';
 import styled from 'styled-components';
 
 const Timer = () => {
-  // let newMins, newSecs;
   const [isPaused, setIsPaused] = useState(true);
-  // const [isShowTimer, setShowTimer] = useState(true);
-  // const [minutes, setMinutes] = useState('00');
+  const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [timeUp, setTimeUp] = useState(false);
+  const [edit, setEdit] = useState(true);
 
-  // const changedTime = () => {
-  //   console.log("focused")
-  // }
-
-  // const setTimer = (e) => {
-  //   setSeconds(e.target.value);
-  //   console.log(seconds);
-  // }
-  const timerHandler = () => {
-    setIsPaused(!isPaused);
+  const resetTimer = () => {
+    setTimeUp(false)
+    setEdit(true);
   }
 
+  const timer = () => {
+    setIsPaused(!isPaused)
+    setEdit(false)
+  }
   let interval = setInterval(()=>{
     if(!isPaused){
       clearInterval(interval);
         if (seconds === 0){
-          setIsPaused(true);
-          // if (minutes !== 0 ){
-          //   setSeconds(59);
-          //   setMinutes(minutes - 1)
-          // }else{
-          //   setSeconds(0);
-          //   setMinutes(0);
-          //   clearInterval(interval);
-          //   setShowTimer(!isShowTimer);
-          //   return setIsPaused(!isPaused);
-          // }
+          if (minutes !== 0 ){
+            setSeconds(59);
+            setMinutes(minutes - 1)
+          }else{
+            setSeconds(0);
+            setMinutes(0);
+            clearInterval(interval);
+            setIsPaused(true);
+            setTimeUp(true);
+          }
         }else{
           setSeconds(seconds - 1)
         }
     }else{
       clearInterval(interval);
     }
-  },1000) 
-
+  },1000)
 
   return(
     <TimerBox>
-      <Time itemType='number' contentEditable={true} suppressContentEditableWarning={true} onBlur={(e) => setSeconds(e.target.innerText)}>
-        {seconds}
-      </Time>
-      <SettingBlock>
+      <Display>
+        {timeUp ? 
+          <TimeUpMsg>Time's Up 🎉</TimeUpMsg>
+        :
+          <TimeBlock>
+            <Time itemType='number' contentEditable={edit} suppressContentEditableWarning={true} onBlur={(min) => setMinutes(min.target.innerText)}>
+              {minutes}
+            </Time>
+            <span>:</span>
+            <Time id='sec' itemType='number' contentEditable={edit} suppressContentEditableWarning={true} onBlur={(sec) => setSeconds(sec.target.innerText)}>
+              {seconds}
+            </Time>
+          </TimeBlock>
+        }
+      </Display>
         <ButtonsBlock>
-          <Button style={{color:"black"}} onClick={timerHandler}>
-            {isPaused? "start" : "stop"}
-          </Button>
+          {
+            timeUp ? 
+            <Button style={{color:"black"}} onClick={resetTimer} >Reset</Button>
+            :
+            <Button style={{color:"black"}} onClick={timer}>
+              {isPaused? "start" : "stop"}
+            </Button>
+          }
         </ButtonsBlock>
-      </SettingBlock>
     </TimerBox>
   )
 };
@@ -63,50 +73,27 @@ const Timer = () => {
 const TimerBox = styled.div`
 margin: auto;
 display: flex;
-justify-content: space-between;
+justify-content: center;
 align-items: center;
 flex-direction: column;
 height: inherit;
 `
-const Time = styled.div`
+const Display = styled.div`
 font-size: 7em;
+`
+const TimeUpMsg = styled.div`
+font-weight: 200;
+`
+const TimeBlock = styled.div`
+display: flex;
+`
+const Time = styled.div`
 margin: auto;
+border: 1px solid transparent;
+box-shadow: none;
+outline: none;
+caret-color: rgba(255,255,255,0.3);
 `
-// const TimeInput = styled.input`
-// margin: auto;
-// font-size: inherit;
-// width: 200px;
-// background: transparent;
-// border: none;
-// box-shadow: none;
-// `
-const SettingBlock = styled.div`
-margin: 0rem;
-`
-// const InputBlock = styled.div`
-// margin: auto;
-// `
-// const InputTime = styled.input`
-// text-align: center;
-// font-size: 1.5em;
-// color: white;
-// height: 2rem;
-// width: 8rem;
-// margin: 0.5rem;
-// border-width: 0px;
-// border-bottom: 1.5px solid black;
-// background-color: transparent;
-
-// :: placeholder{
-//   color: white;
-//   text-align: center;
-// }
-
-// &:focus {
-//   border-bottom: 1.5px solid white;
-//   outline: transparent none 0px;
-// }
-// `
 const ButtonsBlock = styled.div`
 margin: 1rem 0rem;
 display: flex;
